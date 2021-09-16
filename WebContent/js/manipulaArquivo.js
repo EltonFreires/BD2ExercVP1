362e4080656513028e49a66e501c115c38e106a2
Elton Freires <elton.freires@gmail.com>
2021-09-15 22:48:17 -0300

    primeiro commit

---
+++ WebContent/js/manipulaArquivo.js
@@ -0,0 +1,86 @@
+var leitorDeCSV = new FileReader();
+var leitorDeCSV2 = new FileReader();
+
+var totalArquivoTipo1 = 0;
+var totalArquivoTipo2 = 0;
+
+window.onload = function init() {
+	leitorDeCSV.onload = leCSV;
+	leitorDeCSV2.onload = leCSV2;
+	
+}
+
+function pegaCSV(inputFile) {
+	var file = inputFile.files[0];
+	leitorDeCSV.readAsText(file);
+}
+
+function pegaCSV2(inputFile) {
+	var file = inputFile.files[0];
+	leitorDeCSV2.readAsText(file);
+}
+
+function leCSV(evt) {
+
+	var fileArr = evt.target.result.split('\n');
+	var strDiv = '<table>';
+	let ind;
+	totalArquivoTipo1 = 0;
+
+	for (var i = 0; i < fileArr.length; i++) {
+		strDiv += '<tr>';
+		var fileLine = fileArr[i].split(';');
+
+		if (i === 0) { ind = fileLine.indexOf("saldo"); }
+		totalArquivoTipo1 +=  Number.isInteger(parseInt(fileLine[ind])) ? parseInt(fileLine[ind]) : 0;
+		
+		for (var j = 0; j < fileLine.length; j++) {
+			strDiv += '<td>' + fileLine[j].trim() + '</td>';
+		}
+		strDiv += '</tr>';
+	}
+
+
+	// Imprime total 2
+	strDiv += '<tr>';
+	strDiv += '<td>Total</td>';
+	strDiv += '<td>' + totalArquivoTipo1 + '</td>';
+	strDiv += '</tr>';
+
+	strDiv += '</table>';
+
+	var CSVsaida = document.getElementById('CSVsaida');
+	CSVsaida.innerHTML = strDiv;
+}
+
+function leCSV2(evt) {
+
+	var fileArr = evt.target.result.split('\n');
+	var strDiv = '<table>';
+	let ind;
+
+	totalArquivoTipo2 = 0;
+
+	for (var i = 0; i < fileArr.length; i++) {
+		strDiv += '<tr>';
+		var fileLine = fileArr[i].split(';');
+		
+		if (i === 0) { ind = fileLine.indexOf("saldo"); }
+		totalArquivoTipo2 +=  Number.isInteger(parseInt(fileLine[ind])) ? parseInt(fileLine[ind]) : 0;
+		
+		for (var j = 0; j < fileLine.length; j++) {
+			strDiv += '<td>' + fileLine[j].trim() + '</td>';
+		}
+		strDiv += '</tr>';
+	}
+
+	// Imprime total 2
+	strDiv += '<tr>';
+	strDiv += '<td>Total</td>';
+	strDiv += '<td>' + totalArquivoTipo2 + '</td>';
+	strDiv += '</tr>';
+	strDiv += '</table>';
+
+	var CSVsaida = document.getElementById('CSVsaida2');
+	CSVsaida.innerHTML = strDiv;
+}
